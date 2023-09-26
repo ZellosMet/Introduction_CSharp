f97dacda2c7306f4eac408b25d0a1301ad5ef25b
Professional <Professional@USER-PC>
2023-09-26 16:46:54 +0900

    Calculator and Convert DONE

---
+++ Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите выражение: "); 
+            string exampel = Console.ReadLine();
+            string[] slicing = exampel.Split(' ');
+            double lvalue = Convert.ToDouble(slicing[0]);
+            double rvalue = Convert.ToDouble(slicing[2]);
+
+            switch (slicing[1])
+            {
+                case "+":
+                Console.WriteLine(exampel + " = " + (lvalue + rvalue));                    
+                break; 
+                
+                case "-":
+                Console.WriteLine(exampel + " = " + (lvalue - rvalue));                    
+                break;
+
+				case "*":
+				Console.WriteLine(exampel + " = " + (lvalue * rvalue));
+				break;
+
+				case "/":
+				Console.WriteLine(exampel + " = " + (lvalue / rvalue));
+				break;
+			}
+        }
+    }
+}
---
+++ Convert/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+	internal class Program
+	{
+	static readonly string delim = "\n---------------------------------------------------------\n";
+		static void Main(string[] args)
+		{
+			
+			Console.WriteLine("Преобразование числа в денежный формат");
+			Console.Write("Введите дробное число -> ");
+			string num = Console.ReadLine();
+			string[] slicing_num = num.Split('.', ',');
+			Console.WriteLine(num + " грн. - это " + slicing_num[0] + " грн. " + slicing_num[1] + " коп.");
+			Console.WriteLine(delim);
+
+			Console.WriteLine("Вычисление стоимости покупки.");
+			Console.WriteLine("Введите исходные данные:");
+			Console.Write("Цену тетради (грн.) -> ");
+			double notebook_price = Convert.ToDouble(Console.ReadLine());
+			Console.Write("Количество тетрадей -> ");
+			int number_of_notebook = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Цену карандаша (грн.) -> ");
+			double pencil_price = Convert.ToDouble(Console.ReadLine());
+			Console.Write("Количество карандашей -> ");
+			int number_of_pencils = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Стоимость покупки: " + ((notebook_price * number_of_notebook) + (pencil_price * number_of_pencils)) + " грн.");
+			Console.WriteLine(delim);
+
+			Console.WriteLine("Вычисление стоимости покупки.");
+			Console.WriteLine("Введите исходные данные:");
+			Console.Write("Цену тетради (грн.) -> ");
+			notebook_price = Convert.ToDouble(Console.ReadLine());
+			Console.Write("Цену обложки (грн.) -> ");
+			double cover_price = Convert.ToDouble(Console.ReadLine());
+			Console.Write("Количество комплектов (шт.) -> ");
+			int number_of_sets = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Стоимость покупки: " + ((notebook_price + cover_price) * number_of_sets) + " грн.");
+			Console.WriteLine(delim);
+			
+
+			Console.WriteLine("Вычисление стоимости поездки на дачу и обратно");
+			Console.Write("Расстояние до дачи (км) -> ");
+			double distance = Convert.ToDouble(Console.ReadLine());
+			Console.Write("Расход бензина (литров на 100 км пробега) -> ");
+			double consumption = Convert.ToDouble(Console.ReadLine());
+			Console.Write("Цена литра бензина (грн.) -> ");
+			double oil_price = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Поездка на дачу и обратно обойдётся в " + (consumption/100*distance*oil_price*2) + " грн.");
+			Console.WriteLine();
+		}
+	}
+}

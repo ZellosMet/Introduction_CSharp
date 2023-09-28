dffed9413f46aa430b315d3b608101003628d420
Professional <Professional@USER-PC>
2023-09-28 11:08:53 +0900

    Base_array_check

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define BASE_ARRAY_CHECK
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -6,26 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Arrays
-{
+{	
 	internal class Program
 	{
 		static readonly string delim = "\n-------------------------------------------------------------------------\n";
 		static void Main(string[] args)
 		{
+			Random rand = new Random(0); // Создаётся объект класса Random для генерации случайного числа. В скобках задаётся сид
+
+#if BASE_ARRAY_CHECK
+
 			Console.Write("Введите размер массива -> ");
 			int n = Convert.ToInt32(Console.ReadLine());
 			int[] array = new int[n];
-			Random rand = new Random(0); // Создаётся объект класса Random для генерации случайного числа. В скобках задаётся сид
 
 			for (int i = 0; i < array.Length; i++)
 				array[i] = rand.Next(100); // Задание предела для рандома
 
-			for(int i = 0; i < array.Length; i++)
+			for (int i = 0; i < array.Length; i++)
 				Console.Write(array[i] + "\t");
 
 			Console.WriteLine();
-			foreach(int i in array)
-				Console.Write(i+"\t");
+			foreach (int i in array)
+				Console.Write(i + "\t");
 
 			Console.WriteLine(delim);
 
@@ -40,9 +45,9 @@ namespace Arrays
 					doubl_array[i, j] = rand.Next(100);
 
 			for (int i = 0; i < doubl_array.GetLength(0); i++)
-			{ 
+			{
 				for (int j = 0; j < doubl_array.GetLength(1); j++)
-					Console.Write(doubl_array[i,j] + "\t");
+					Console.Write(doubl_array[i, j] + "\t");
 				Console.WriteLine();
 			}
 
@@ -57,8 +62,8 @@ namespace Arrays
 				};
 
 			for (int i = 0; i < jagget_array.Length; i++)
-			{ 
-				for(int j = 0; j< jagget_array[i].Length; j++)
+			{
+				for (int j = 0; j < jagget_array[i].Length; j++)
 					Console.Write(jagget_array[i][j] + "\t");
 				Console.WriteLine();
 			}
@@ -69,11 +74,22 @@ namespace Arrays
 				{
 					doubl_array,
 					new int[,]
-					{ 
+					{
 						{ 256, 384, 512, 768},
 						{ 1024, 2048, 3072, 4096}
 					}
 				};
+
+			//Вывод зубодробительного массива
+			for (int i = 0; i < jagget_array_2.Length; i++)
+				for (int j = 0; j < jagget_array_2[i].GetLength(0); j++)
+				{
+					for (int l = 0; l < jagget_array_2[i].GetLength(1); l++)
+						Console.Write(jagget_array_2[i][j, l] + "\t");
+					Console.WriteLine();
+				}
+			Console.WriteLine(); 
+#endif
 		}
 	}
 }
